3e79d8df34b35a11c6a19ecc3af4376afead8095
tysonbivens-design <tysonbivens@gmail.com>
2026-03-08 16:29:25 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,19 @@
-tset
+import './globals.css'
+
+export const metadata = {
+  title: 'Tyson Reid',
+  description: 'Presence is resistance. Curation is revolutionary.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400&family=Playfair+Display:ital,wght@0,400;0,700;0,900;1,400;1,700&family=VT323&family=Courier+Prime:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
